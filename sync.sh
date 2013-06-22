b35aced506a5e6e5e0d285a4454262f6e5102cff
ike3 <ike@email.org>
2013-06-22 06:52:25 +0400

    r2 -> Wetlands

--- sync.sh
+++ sync.sh
@@ -2,11 +2,13 @@
 JAVA_CMD="java -cp target/playerbot-transfer-utils-0.0.1-SNAPSHOT-with-dependencies.jar org.playerbot.ai.Main"
 JAVA_OPTS=""
 CMDLINE_PARAMS="-mode update -config src/main/resources/config.xml"
+#CMDLINE_PARAMS="-mode replace -config src/main/resources/config.xml"
 CMDLINE="$JAVA_CMD $JAVA_OPTS $CMDLINE_PARAMS"
 
 cd playerbot-utils/transfer
 
-$CMDLINE -source r2_Eversong -destination r2_Wetlands -name Cali -name Eleya -name Siri -name Alaine
+$CMDLINE -source r2_Eversong -destination r2_Wetlands -name Cali -name Eleya -name Siri -name Alaine -name Aelas
+#$CMDLINE -source r2_Eversong -destination r2_Wetlands -name Aelas
 
 echo "Done. Press ENTER"
 read
