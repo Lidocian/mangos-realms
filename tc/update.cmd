5f0183ec685b41ddf7936121850db2a66c6f28ed
ike3 <ike@email.org>
2015-12-30 07:10:08 +0300

    tc -> Windows

--- tc/update.cmd
+++ tc/update.cmd
@@ -1,1 +1,6 @@
 xcopy /F /Y Testing\worldserver.exe Eversong
+xcopy /F /Y Testing\worldserver.exe Wetlands
+xcopy /F /Y Testing\worldserver.exe Stranglethorn
+xcopy /F /Y Testing\worldserver.exe Tanaris
+xcopy /F /Y Testing\worldserver.exe Plaguelands
+xcopy /F /Y Testing\worldserver.exe Outland
